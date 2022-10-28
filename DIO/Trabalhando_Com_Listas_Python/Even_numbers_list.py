9d91f9057465cd0868c96045bd89a89efca0e109
Saulo Carvalho <saulodias60@gmail.com>
2022-10-28 17:29:40 -0300

    Listas: Criação e acesso aos dados

---
+++ DIO/Trabalhando_Com_Listas_Python/Even_numbers_list.py
@@ -0,0 +1,11 @@
+numeros = [1,30,21,2,9,65,34]
+#pares = []
+pares = [numero for numero in numeros if numero % 2 == 0]
+#for numero in numeros:
+#    if numero % 2 == 0:
+#        pares.append(numero)
+
+print(pares)
+
+
+
